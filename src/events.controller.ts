974799569109d3715ae9e6ffe318e3ff39486c3e
manzitresor <manzitre@gmail.com>
2024-10-07 16:45:26 +0200

    chore: add validation

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -4,32 +4,55 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateEventsDto } from './create.events.dto';
 import { UpdateEventsDto } from './update.events.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from './event.entity';
 
 @Controller('events')
 export class EventsController {
+  constructor(
+    @InjectRepository(Event)
+    private readonly repository: Repository<Event>,
+  ) {}
+
   @Get()
-  getAll() {
-    return 'All events';
+  async getAll() {
+    return await this.repository.find();
   }
+
   @Get(':id')
-  findOne(@Param('id') id) {
-    return `Get single event ${id}`;
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.repository.findOne({ where: { id } });
   }
+
   @Post()
-  create(@Body() input: CreateEventsDto): CreateEventsDto {
-    return input;
+  async create(@Body(ValidationPipe) input: CreateEventsDto) {
+    return await this.repository.save({
+      ...input,
+      when: new Date(input.when),
+    });
   }
+
   @Patch(':id')
-  update(@Param('id') id, @Body() input: UpdateEventsDto) {
-    return `Update event ${id} ${input.when}`;
+  async update(@Param('id') id, @Body() input: UpdateEventsDto) {
+    const event = await this.repository.findOne(id);
+    return await this.repository.save({
+      ...event,
+      ...input,
+      when: input.when ? new Date(event.when) : event.when,
+    });
   }
+
   @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return `Delete event ${id}`;
+  async remove(@Param('id') id) {
+    const event = await this.repository.findOne(id);
+    await this.repository.remove(event);
   }
 }
